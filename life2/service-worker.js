3280c2a419fbfceba44c476e3f0ae3f57047d9fe
Kumassy <kumassyii@gmail.com>
2016-10-27 00:43:55 +0900

    fix

--- life2/service-worker.js
+++ life2/service-worker.js
@@ -29,7 +29,7 @@
 
 
 /* eslint-disable quotes, comma-spacing */
-var PrecacheConfig = [["/src/life-app/life-app.html","ff8ba05e8e9a18a6a11d4da3a264d43d"],["/src/life-canvas/life-canvas.html","379a20b627e1aab4936c8d1749458233"]];
+var PrecacheConfig = [["/index.html","947cb40090fb18fab489d93010d52f8e"],["/src/life-app/life-app.html","ff8ba05e8e9a18a6a11d4da3a264d43d"],["/src/life-canvas/life-canvas.html","379a20b627e1aab4936c8d1749458233"]];
 /* eslint-enable quotes, comma-spacing */
 var CacheNamePrefix = 'sw-precache-v1--' + (self.registration ? self.registration.scope : '') + '-';
 
